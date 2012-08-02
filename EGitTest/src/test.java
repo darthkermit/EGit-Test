11c3bf2814aa9dd610eb046ccf86c87b8b1a20bc
kermit <christoph.hoeppke@googlemail.com>
2012-08-02 11:42:30 +0200

    uploading second test

--- EGitTest/src/test.java
+++ EGitTest/src/test.java
@@ -5,7 +5,10 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		newtest mytest = new newtest("Bacon");
 		System.out.println("GitHub Test");
+		
+		mytest.doit();
 
 	}
 
